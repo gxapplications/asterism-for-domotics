783d9855b3739567c25d5d15364b58652cc8b10a
Xavier Gouley <xavier.gouley@gmail.com>
2020-01-20 14:44:09 +0100

    try setup:build with notifs

--- setup-build.js
+++ setup-build.js
@@ -8,9 +8,21 @@ const packageData = require('./package.json')
 const release = packageData.version
 console.log(('Asterism for domotics release '+release+' will now build!').cyan)
 
-const asterism = require('asterism')
-const server = asterism.server
-const browser = asterism.browser
+// Get setup params
+const setupData = require('./setup.json')
+const portOffset = setupData.mode === 3 ? 0 : 9000
+const modeLog = (setupData.mode === 3) ?
+    'Asterism for domotics running on Internet, ports 80/443, available to the entire world!'.yellow :
+    'Asterism for domotics running on localhost, ports 9080/9443, available from local network!'.green
+
+if (setupData.mode > 1) { // Only HTTPS, WebPush notifications
+  process.argv.push(`--webPushPublicKey=${setupData.webPushPublicKey}`)
+  process.argv.push(`--webPushPrivateKey=${setupData.webPushPrivateKey}`)
+  process.argv.push(`--webPushEmail=mailto:${setupData.email}`)
+  process.argv.push(`--webPushServerUrl=${setupData.domains[0]}`)
+}
+
+const { server, browser } = require('asterism')
 
 // Plugins
 const plugins = packageData['asterism-plugins']
