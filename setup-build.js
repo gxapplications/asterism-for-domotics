128575f24be5bdae18740ae88df52f3981920762
Xavier Gouley <xavier.gouley@gmail.com>
2020-04-17 02:06:25 +0200

    build flag to avoid notifs in build mode

--- setup-build.js
+++ setup-build.js
@@ -8,13 +8,14 @@ const packageData = require('./package.json')
 const release = packageData.version
 console.log(('Asterism for domotics release '+release+' will now build!').cyan)
 
-// Get setup params
 const setupData = require('./setup.json')
 if (setupData.mode > 1) { // Only HTTPS, WebPush notifications
-  // notif deactivated with 'build' flag
-  process.argv.push(`--webPushServerUrl=build`)
+  process.argv.push(`--webPushPublicKey=${setupData.webPushPublicKey}`)
+  process.argv.push(`--webPushPrivateKey=${setupData.webPushPrivateKey}`)
+  process.argv.push(`--webPushEmail=mailto:${setupData.email}`)
+  process.argv.push(`--webPushServerUrl=${setupData.domains[0]}`)
 }
-
+process.argv.push(`--build-only=true`)
 const { server, browser } = require('asterism')
 
 // Plugins
